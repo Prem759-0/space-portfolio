7957b17c3a5777a149c88e3fc2efdb985bacf785
premagikwad1213 <a70064182@gmail.com>
2026-06-11 19:06:01 +0530

    Update footer, navbar, and site title to reflect correct author name

--- components/main/footer.tsx
+++ components/main/footer.tsx
@@ -30,7 +30,7 @@ export const Footer = () => {
         </div>
 
         <div className="mb-[20px] text-[15px] text-center">
-          &copy; John doe {new Date().getFullYear()} Inc. All rights reserved.
+          &copy; Prem Gaikwad {new Date().getFullYear()} Inc. All rights reserved.
         </div>
       </div>
     </div>
--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "John doe | Space Portfolio",
+  title: "Prem Gaikwad | Space Portfolio",
   description: "Welcome to my full stack Next.js 14 space portfolio.",
   keywords: [
     "reactjs",
